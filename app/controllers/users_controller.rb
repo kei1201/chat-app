de449c35824a672e4a8a24244a0d5e1ee4636529
kei1201 <k16180339@icloud.com>
2021-05-19 18:29:54 +0900

    最終チェック

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   def edit
   end
 
@@ -7,11 +7,12 @@ class UsersController < ApplicationController
     if current_user.update(user_params)
       redirect_to root_path
     else
-    render :edit
+      render :edit
     end
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :email)
   end
